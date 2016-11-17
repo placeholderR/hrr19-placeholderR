e9b5f4027f9d2cf213dec2078f2ecdbdd1871a74
jkennedy3161 <kennedyjt88@gmail.com>
2016-11-16 23:45:25 -0800

    'header component'

---
+++ src/studiosHeader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class StudiosHeader extends React.Component {
+  render() {
+    return(
+
+        <thead>
+          <tr>
+            <th>Tasks</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+    )
+  }
+}
