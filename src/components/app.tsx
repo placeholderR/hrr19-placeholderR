ddefd04780c9814b4ed346b8a03c3f09f1043e90
jkennedy3161 <kennedyjt88@gmail.com>
2016-11-16 21:32:29 -0800

    'removed traces of TypeScript'

--- src/components/app.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export interface IApp {};
-
-export class App extends React.Component<IApp, {}> {
-  render() {
-    return <h1>StageUp - Track your studio orders with ease!</h1>;
-  }
-}
