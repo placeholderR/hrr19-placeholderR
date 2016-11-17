c0a5fa3088cf2ff1e0724e6d63162eb9fb770a09
jkennedy3161 <kennedyjt88@gmail.com>
2016-11-17 03:57:04 -0800

    'ticket component retrieving data fields from clicking the add'

--- src/ticket.js
+++ src/ticket.js
@@ -12,16 +12,41 @@ export default class Ticket extends React.Component {
   render() {
     return(
       <div style={style}>
-        <form>
-          <input type='text' placeholder='School Name here' />
-          Date<input type='date' />
-          Group<input type='radio' />
-          Composite<input type='radio' />
+        <form onSubmit={this.handleCreate.bind(this)}>
+          Order ID
+          <input type='text' placeholder='School Name ID#' ref='createInput'/>
+          Order Date<input type='date' ref='createDate1'/>
+          Makeup Date<input type='date' ref='createDate2'/>
+          Group<input type='radio' ref='createGroup'/>
+          Composite<input type='radio' ref='createComp'/>
           <br />
-          Rush<input type='radio' />
+          Rush<input type='radio' ref='createRush'/>
+          <button>Add Ticket</button>
         </form>
-        <button>Add Ticket</button>
+
       </div>
     )
+  }
+  handleCreate(e) {
+    e.preventDefault();
+
+    this.props.createTicket(
+      {
+        name: this.refs.createInput.value,
+        date1: this.refs.createDate1.value,
+        date2: this.refs.createDate2.value,
+        group: this.refs.createGroup.checked,
+        comp: this.refs.createComp.checked,
+        rush: this.refs.createRush.checked
+      }
+    );
+    // resetting fields
+    this.refs.createInput.value = '';
+    this.refs.createDate1.value = '';
+    this.refs.createDate2.value = '';
+    this.refs.createGroup.checked = false;
+    this.refs.createComp.checked = true;
+    this.refs.createRush.checked = false;
+
   }
 }
