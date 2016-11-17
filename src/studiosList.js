90657e2bddb63e5392cbbd895efa701a20636424
jkennedy3161 <kennedyjt88@gmail.com>
2016-11-16 23:44:56 -0800

    'component for showing different studio profiles'

---
+++ src/studiosList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StudiosHeader from './studiosHeader'
+
+export default class StudiosList extends React.Component {
+  render() {
+    return(
+      <table>
+        <StudiosHeader />
+      </table>
+    )
+  }
+}
