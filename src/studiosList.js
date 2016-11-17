517b8d40b2f7fb6d15678cb999a40ffb1bed36cf
jkennedy3161 <kennedyjt88@gmail.com>
2016-11-17 01:45:34 -0800

    'uses lodash to pass each studio object into studiosListItem component'

--- src/studiosList.js
+++ src/studiosList.js
@@ -1,11 +1,19 @@
+import _ from 'lodash';
 import React from 'react';
 import StudiosHeader from './studiosHeader'
+import StudiosListItem from './studiosListItem'
 
 export default class StudiosList extends React.Component {
+  renderItems() {
+    return _.map(this.props.studios, (studio, index) => <StudiosListItem key={index} {...studio} />);
+  }
   render() {
     return(
       <table>
         <StudiosHeader />
+        <tbody>
+          {this.renderItems()}
+        </tbody>
       </table>
     )
   }
