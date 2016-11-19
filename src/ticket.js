5f446c42aa8ec347e70f2280dc5dddfdc4edcc38
jkennedy3161 <kennedyjt88@gmail.com>
2016-11-18 16:05:06 -0800

    This commit will add color style to when a order is selected as rush.

--- src/ticket.js
+++ src/ticket.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const style = {
-  height: '100px',
+  height: '150px',
   width: '200px',
   backgroundColor: '#bbb',
   padding: '3em',
@@ -17,10 +17,12 @@ export default class Ticket extends React.Component {
           <input type='text' placeholder='School Name ID#' ref='createInput'/>
           Order Date<input type='date' ref='createDate1'/>
           Makeup Date<input type='date' ref='createDate2'/>
+          <br />
           Group<input type='radio' ref='createGroup'/>
           Composite<input type='radio' ref='createComp'/>
           <br />
-          Rush<input type='radio' ref='createRush'/>
+          <span style={{color: 'red'}}>Rush</span><input type='radio' ref='createRush'/>
+          <br />
           <button>Add Ticket</button>
         </form>
 
@@ -45,7 +47,7 @@ export default class Ticket extends React.Component {
     this.refs.createDate1.value = '';
     this.refs.createDate2.value = '';
     this.refs.createGroup.checked = false;
-    this.refs.createComp.checked = true;
+    this.refs.createComp.checked = false;
     this.refs.createRush.checked = false;
 
   }
