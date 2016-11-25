a65051d49abbad583d811bef92955668044a44e6
jkennedy3161 <kennedyjt88@gmail.com>
2016-11-24 23:46:37 -0800

    create for storing database for cleardb or local db connecting

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,6 @@
 var heroku = {
-  HEROKU_MYSQL_URI: 'mysql://bc3f8066e34a5b:fbeb9e19@us-cdbr-iron-east-04.cleardb.net/heroku_37c9d777733e780?reconnect=true'
+  HEROKU_MYSQL_URI: 'mysql://bbbd4130019141:25337937@us-cdbr-iron-east-04.cleardb.net/heroku_ec16491ee51b22f?reconnect=true',
+  LOCAL_DB: 'mysql://root:@localhost/stageup'
 };
 
 module.exports = heroku;
