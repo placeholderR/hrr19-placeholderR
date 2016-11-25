37f6cacf66c3f62231e1db60636fc0bdace8b79b
jkennedy3161 <kennedyjt88@gmail.com>
2016-11-25 00:44:55 -0800

    add home component page

---
+++ client/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>stageUp - Track your order stages with ease!</h1>
+        <div className=''>
+          <h3>Meet the Team</h3>
+          <p>Jarrett Kennedy</p>
+          <img style={{width: '100px', height: '100px'}} src='https://avatars1.githubusercontent.com/u/15637516?v=3&s=460' />
+        </div>
+      </div>
+    );
+  }
+}
