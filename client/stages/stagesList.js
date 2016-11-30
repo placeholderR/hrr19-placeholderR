d5150463322c558bc95530b3332e6c0505d179f5
jkennedy3161 <kennedyjt88@gmail.com>
2016-11-29 16:04:56 -0800

    remove temporary stages for newly created ones

--- client/stages/stagesList.js
+++ client/stages/stagesList.js
@@ -19,35 +19,9 @@ export class StagesList extends React.Component {
     if (this.props.tickets.length !== 0) {
       return(
         <div id='stage' style={{'padding': '5em'}}>
-          <div style={style}>
-            <span>Image Match</span>
-            {_.map(this.props.tickets, (ticket, index) => {
-              return <StagesListItem key={index} ticket={ticket} />
-            })}
-          </div>
-          <div style={style}>
-            <span>DP2</span>
-          </div>
-          <div style={style}>
-            <span>Color Correction</span>
-          </div>
-          <br />
-          <div style={style}>
-            <span>Packaging</span>
-          </div>
-          <div style={style}>
-            <span>Printing</span>
-          </div>
-          <div style={style}>
-            <span>ID Laminate</span>
-          </div>
-          <br />
-          <div style={style}>
-            <span>Done</span>
-          </div>
-          <div style={style}>
-            <span>Invoiced</span>
-          </div>
+        {_.map(this.props.tickets, (ticket, index) => {
+          return <StagesListItem key={index} ticket={ticket} />
+        })}
         </div>
       );
     } else {
