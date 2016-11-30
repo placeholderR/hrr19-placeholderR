f51f640e6abba687a8a7ca2cddc38ec564b42aed
jkennedy3161 <kennedyjt88@gmail.com>
2016-11-29 16:05:59 -0800

    add event listening with picking a stage from a option menu, setting up for update

--- client/stages/stagesListItem.js
+++ client/stages/stagesListItem.js
@@ -1,26 +1,64 @@
+import _ from 'lodash';
 import React from 'react';
 
 const style = {
-  height: '5px',
-  width: '70px',
+  height: '100px',
+  width: '300px',
   backgroundColor: '',
   padding: '1em',
   display: 'inline-block',
-  fontSize: '9px',
+  fontSize: '12px',
   fontWeight: 'bold',
-  fontFamily: 'sans-serif'
+  fontFamily: 'sans-serif',
+  listStyle: 'none'
+};
+
+const style2 = {
+  'backgroundColor': 'grey',
+  'cursor': 'pointer',
+  'height': '150px',
+  'width': '690px',
+  'display': 'inline-table',
+  'padding': '2em',
+  'border': '2px solid black',
+  'textAlign': 'center'
 };
 
 const label = {
   option: ''
 };
 
+const names = ['DP2', 'Image Match', 'Color Correction', 'Printing', 'ID Laminate', 'Done', 'Invoiced', 'Packaging'];
+
 export class StagesListItem extends React.Component {
   render() {
     style.backgroundColor = this.props.ticket.rush === true ? '#ce2323' : '#bbb';
     label.option = this.props.ticket.group === true ? ' G' : this.props.ticket.comp === true ? ' C' : '';
+    //console.log(names);
     return (
-      <li style={style} id={this.props.ticket.id}>{this.props.ticket.name + label.option}</li>
+      <div style={style2}>
+      <span>{this.props.ticket.stage}</span>
+        <ul id={this.props.ticket.id} style={style}>
+          <li>{this.props.ticket.name + label.option}</li>
+          <li>{this.props.ticket.date}</li>
+          <li>{this.props.ticket.date2}</li>
+        </ul>
+        <div>
+          <select ref='ChangeStage'>
+          {_.map(names, (name, index) => {
+
+            return <option key={index}>{name}</option>
+          })}
+          }
+          </select>
+          <button onClick={this.changeStage.bind(this)}>Move</button>
+        </div>
+      </div>
     );
+  }
+  changeStage(e) {
+    e.preventDefault();
+
+    //console.log(this.refs.ChangeStage.value);
   }
 }
