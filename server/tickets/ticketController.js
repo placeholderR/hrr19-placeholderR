458e82c5c0f79c03d518275e1a7c6b7b87c44f2f
jkennedy3161 <kennedyjt88@gmail.com>
2016-12-01 08:20:40 -0800

    add PUT and DELETE methods for ticket controller

--- server/tickets/ticketController.js
+++ server/tickets/ticketController.js
@@ -25,5 +25,43 @@ module.exports = {
       //console.log(req.body.name, 'req');
       res.sendStatus(201);
     });
+  },
+  put: function (req, res) {
+    db.Ticket.update(
+      {
+        name: req.body.name,
+        date: req.body.date,
+        date2: req.body.date2,
+        group: req.body.group,
+        comp: req.body.comp,
+        rush: req.body.rush,
+        stage: req.body.stage
+      },
+      {
+        fields: ['name', 'date', 'date2', 'group', 'comp', 'rush', 'stage'],
+        where: {id: req.params.id}
+      }
+    )
+    .then( ticket => {
+      res.sendStatus(200);
+    })
+  },
+  getId: function (req, res) {
+    db.Ticket.find(
+      {
+        where: {id: req.params.id}
+      })
+    .then(ticket => {
+      res.json(ticket);
+    })
+  },
+  remove: function (req, res) {
+    db.Ticket.destroy(
+      {
+        where: {id: req.params.id}
+      })
+    .then(ticket => {
+      res.sendStatus(200);
+    })
   }
 };
