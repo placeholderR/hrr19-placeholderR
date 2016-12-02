ee5dec8f98232f6ef55eb9cf1dcf2c438efdd326
jkennedy3161 <kennedyjt88@gmail.com>
2016-12-01 19:23:35 -0800

    add states for ticket fields, to change with new inputs

--- client/stages/stagesListItem.js
+++ client/stages/stagesListItem.js
@@ -32,13 +32,31 @@ var label = {
 
 var names = ['DP2', 'Image Match', 'Color Correction', 'Printing', 'ID Laminate', 'Done', 'Invoiced', 'Packaging'];
 
+
 export class StagesListItem extends React.Component {
   // constructor method
   constructor(props) {
     super(props);
     // create the state isEditable and default to false for switching to true later
     // after the edit button is clicked
-    this.state = {isEditable : false};
+    this.state = {
+      isEditable : false,
+      date: this.props.ticket.date,
+      date2: this.props.ticket.date2,
+      name: this.props.ticket.name
+    };
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleDate2Change = this.handleDate2Change.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+  handleDateChange(e) {
+    this.setState({date: e.target.value});
+  }
+  handleDate2Change(e) {
+    this.setState({date2: e.target.value});
+  }
+  handleNameChange(e) {
+    this.setState({name: e.target.value});
   }
   render() {
     style.backgroundColor = this.props.ticket.rush === true ? '#ce2323' : '#bbb';
@@ -48,15 +66,28 @@ export class StagesListItem extends React.Component {
       //console.log('stage ' + this.props.ticket.stage + ' already exists');
 
     }
+    var regex = new RegExp('-', 'g');
+
+    var date = this.state.date;
+    var date2 = this.state.date2;
+    var year = date.slice(0,4);
+    var year2 = date2.slice(0,4);
+    var monthDay = date.slice(-5);
+    var monthDay2 = date2.slice(-5);
+    var newDate = monthDay + '-' + year;
+    var newDate2 = monthDay2 + '-' + year2;
+    newDate.replace(regex, '/');
+    newDate2.replace(regex, '/');
     // if !isEditable
+    //console.log(this.state.isEditable);
     if (!this.state.isEditable) {
       return (
         <div id={this.props.ticket.stage + this.props.ticket.id}style={style2}>
         <span style={{textAlign: 'center', display: 'block'}}>{this.props.ticket.stage}</span>
-          <ul onClick={this.moveStage.bind(this)} style={style}>
+          <ul style={style}>
           <li>School Name: {this.props.ticket.name}{label.option}</li>
-          <li>Order Date: {this.props.ticket.date}</li>
-          <li>Makeup Date: {this.props.ticket.date2}</li>
+          <li>Order Date: {this.state.date}</li>
+          <li>Makeup Date: {this.state.date2}</li>
           </ul>
           <div className='container'>
             <select ref='ChangeStage'>
@@ -73,17 +104,34 @@ export class StagesListItem extends React.Component {
         </div>
       );
     } else {
-      // else return a div that contains input tags instead of li tags for ticket fields
       return <div id={this.props.ticket.stage + this.props.ticket.id}style={style2}>
         <span style={{textAlign: 'center', display: 'block'}}>{this.props.ticket.stage}</span>
           <form style={style}>
-            School Name: <input type='text' ref='EditName' placeholder={this.props.ticket.name} />
-            Order Date: <input type='date' ref='Date1Name' />{this.props.ticket.date}
-            Makeup Date: <input type='date' ref='Date2Name' />{this.props.ticket.date2}
+            School Name: <input
+            type='text'
+            ref='EditName'
+            value={this.state.name}
+            onChange={this.handleNameChange}
+            />
+            Order Date: <input
+            type='date'
+            ref='Date1Name'
+            value={this.state.date}
+            onChange={this.handleDateChange}
+            />
+            Makeup Date: <input
+            type='date'
+            ref='Date2Name'
+            value={this.state.date2}
+            onChange={this.handleDate2Change}
+            />
           </form>
           <button onClick={this.saveTicket.bind(this)}>Add</button>
         </div>
     }
+    // else
+      // return a div that contains input tags instead of li tags for
+      // ticket fields
   }
   editTicket(e) {
     e.preventDefault();
@@ -93,9 +141,9 @@ export class StagesListItem extends React.Component {
     var edit = this.state.isEditable;
     edit = true;
     this.setState({isEditable: edit});
+
   }
   saveTicket(e) {
-    e.preventDefault();
     // edit a ticket here
     // set the state isEditable to true
     // invoke renderEdit method
@@ -107,9 +155,9 @@ export class StagesListItem extends React.Component {
       method: 'PUT',
       body: JSON.stringify(
         {
-          name: this.refs.EditName.value,
-          date: this.refs.Date1Name.value,
-          date2: this.refs.Date2Name.value
+          name: this.state.name,
+          date: this.state.date,
+          date2: this.state.date2
         }
       )
     })
@@ -119,6 +167,7 @@ export class StagesListItem extends React.Component {
     .catch( err => {
       //console.log(err,'error!');
     })
+
     var save = this.state.isEditable;
     save = false;
     this.setState({isEditable: save});
