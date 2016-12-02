215da8aaa5cc7003760b1c980121f19d9447b618
jkennedy3161 <kennedyjt88@gmail.com>
2016-12-02 15:45:33 -0800

    update ticket with bootstrap style content

--- client/ticket/ticket.js
+++ client/ticket/ticket.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import _ from 'lodash';
-import { Button } from 'react-bootstrap/lib';
 
-const style = {
-  height: '150px',
-  width: '200px',
-  backgroundColor: '#bbb',
-  padding: '3em',
-  marginLeft: '15%'
+var style2 = {
+  'backgroundColor': '#E6EDEB',
+  'cursor': 'pointer',
+  'height': '150px',
+  'width': '400px',
+  'display': 'inline-table',
+  'padding': '2em',
+  'border': '4px solid #81918E',
+  'textAlign': 'center'
 };
 
 var names = ['DP2', 'Image Match', 'Color Correction', 'Printing', 'ID Laminate', 'Done', 'Invoiced', 'Packaging'];
@@ -15,29 +17,53 @@ var names = ['DP2', 'Image Match', 'Color Correction', 'Printing', 'ID Laminate'
 export default class Ticket extends React.Component {
   render() {
     return(
-      <div style={style}>
-        <form onSubmit={this.handleCreate.bind(this)}>
-          Order ID
-          <input type='text' placeholder='School Name ID#' ref='createInput'/>
-          Order Date<input type='date' ref='createDate1'/>
-          Makeup Date<input type='date' ref='createDate2'/>
-          <br />
-          Group<input type='radio' ref='createGroup'/>
-          Composite<input type='radio' ref='createComp'/>
-          <br />
-          <span style={{color: 'red'}}>Rush</span><input type='radio' ref='createRush'/>
-          <br />
-          <select ref='createStage'>
-          <option value=''>Change Stage</option>
-          {_.map(names, (name, index) => {
+      <div className='container text-center' style={style2}>
+        <div className='row'>
+          <div className='col-lg-8 col-lg-offset-2 text center'>
+            <h3>Ticket Creator</h3>
+            <form className='no-padding' onSubmit={this.handleCreate.bind(this)}>
+              Order ID:
+              <input
+              type='text'
+              placeholder='School Name ID#'
+              ref='createInput'/>
+              <br />
+              Order Date:
+              <input
+              type='date'
+              ref='createDate1'/>
+              <br />
+              Makeup Date:
+              <input
+              type='date'
+              ref='createDate2'/>
+              <br />
+              Group:
+              <input
+              type='radio'
+              ref='createGroup'/>
+              Composite<input
+              type='radio'
+              ref='createComp'/>
+              <br />
+              <span
+              style={{color: 'red'}}>Rush</span>
+              <input
+              type='radio'
+              ref='createRush'/>
+              <br />
+              <select ref='createStage'>
+              <option value=''>Change Stage</option>
+              {_.map(names, (name, index) => {
 
-            return (
-              <option key={index} value={name}>{name}</option>)
-          })}
-          </select>
-          <Button bsStyle='primary'>Add Ticket</Button>
-        </form>
-
+                return (
+                  <option key={index} value={name}>{name}</option>)
+              })}
+              </select>
+              <button className='btn btn-primary'>Add Ticket</button>
+            </form>
+          </div>
+        </div>
       </div>
     )
   }
