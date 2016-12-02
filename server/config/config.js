4cffd49d12b1efc3f09a04599a1b8be8146e0ca3
jkennedy3161 <kennedyjt88@gmail.com>
2016-12-01 19:24:21 -0800

    dropped heroku database and updated config var

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 var heroku = {
-  HEROKU_MYSQL_URI: 'mysql://b99e1cfe131e70:9572784e@us-cdbr-iron-east-04.cleardb.net/heroku_6192a6f8d6f0dc4?reconnect=true',
+  HEROKU_MYSQL_URI: 'mysql://b85691807df2bb:26eb60b2@us-cdbr-iron-east-04.cleardb.net/heroku_80edf9cf2bcba41?reconnect=true',
   LOCAL_DB: 'mysql://root:@localhost/stageup'
 };
 
